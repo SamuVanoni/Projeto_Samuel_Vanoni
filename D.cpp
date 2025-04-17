8c4f911a734000a83112161e23e882753e1c3e07
Samuel Vanoni <samu.prog00@gmail.com>
2025-04-17 12:22:56 -0300

    Criação da Classe D

---
+++ D.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class D {
+private:
+    int D1;
+    string D2;
+
+public:
+    void setD1(int d1) { D1 = d1; }
+    void setD2(string d2) { D2 = d2; }
+
+    int getD1() const { return D1; }
+    string getD2() const { return D2; }
+
+    void MD1() {
+        cout << "Método MD1 chamado" << endl;
+    }
+
+    void MD2() {
+        cout << "Método MD2 chamado" << endl;
+    }
+};
