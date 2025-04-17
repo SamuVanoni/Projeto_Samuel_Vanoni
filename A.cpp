b1356d4134bbdcded8383d9a6d23398403281d07
Samuel Vanoni <samu.prog00@gmail.com>
2025-04-17 10:38:54 -0300

    :sparkles: feat: Add Class A.cpp

---
+++ A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class A {
+private:
+    int A1;
+    float A2;
+
+public:
+    int getA1() const { return A1; }
+    float getA2() const { return A2; }
+
+    void setA1(int a1) { A1 = a1; }
+    void setA2(float a2) { A2 = a2; }
+
+    void MA1() { cout << "Método MA1 chamado" << endl; }
+    void MA2() { cout << "Método MA2 chamado" << endl; }
+};
+
+// Exportando a classe para uso externo
+A global_A;
